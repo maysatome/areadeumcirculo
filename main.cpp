a6e897d66d4346da4a3f27e21c869c443b7332b2
maysatome <artemmelnykphd@gmail.com>
2019-02-12 23:48:29 +0000

    fery basic version

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// Faça um programa que receba quatro números inteiros, calcule e mostre a soma desses números.
+
+#include<iostream>
+
+int main()
+{
+
+  int um, dois, tres, quatro;
+ std::cin>>um;
+  std::cin>>dois;
+  std::cin>>tres;
+  std::cin>>quatro;
+  std::cout << um+ dois+ tres+ quatro;
+  return 0;
+}
